26d72f784bd114e4de84a9547ce131f8b3a34cbe
MaxDobbs32 <70861742+MaxDobbs32@users.noreply.github.com>
2020-12-18 19:45:25 -0600

    Update Stronger Proof.rb

--- Proof of Turing Completeness/Stronger Proof.rb
+++ Proof of Turing Completeness/Stronger Proof.rb
@@ -1,5 +1,7 @@
 =begin
 
+The previous document is easier to read and provides context for this program, so I recommend checking it out.
+
 Lakshayati has some features that make it easier to program in. However, if it did not have these features, would it still be
 Turing-complete? To answer this question, I recreated the program found in the other proof in Ruby. For an additional challenge,
 I decided all variable names can only contain the letter "o". The program works as intended, so, even with these contraints,
